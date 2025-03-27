1bb8680592d7f18f135e29e065ed531cb59631ad
lipangang-li <1346541792@qq.com>
2025-03-27 10:07:34 +0800

    修改入口方法名

--- src/main/java/org/example/DelicousFoodApplication.java
+++ src/main/java/org/example/DelicousFoodApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewApplication {
+public class DelicousFoodApplication {
     public static void main(String[] args) {
         // 支持通过命令行参数传递配置
-        SpringApplication.run(NewApplication.class, args);
+        SpringApplication.run(DelicousFoodApplication.class, args);
     }
 }
